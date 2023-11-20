6a68dda3c6a800c761a1309cadacdd08f5971334
unknown <mdshahreerirfan@gmail.com>
2023-11-20 23:17:51 +0600

    All task completed

--- LAB_Task_02/Task_five.java
+++ LAB_Task_02/Task_five.java
@@ -1,7 +1,32 @@
+import java.util.Scanner;
 public class Task_five {
-    
+    // This is maximum funtion
+    static int maxi(int n, int[] arr){
+        int maximum = 0;
+        for(int i=0;i<n;i++){
+            if(arr[i]>maximum){
+                maximum = arr[i];
+            }
+        }
+        return maximum;
+    }
     // This is main function
     public static void main(String[] args) {
-        
+        int n;
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("How many numbers: ");
+
+        n = input.nextInt();
+
+        System.out.print("Enter "+n+" numbers: ");
+
+        int[] arr = new int[n];
+
+        for(int i=0;i<n;i++){
+            arr[i] = input.nextInt();
+        }
+
+        System.out.println("Maximum number is:"+maxi(n, arr));
     }
 }
