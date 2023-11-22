aab972b496214c2308f20246c82076dcae9af16e
unknown <mdshahreerirfan@gmail.com>
2023-11-22 21:58:47 +0600

    End commit

--- LAB_Task_02/Task_five.java
+++ LAB_Task_02/Task_five.java
@@ -30,3 +30,42 @@ public class Task_five {
         System.out.println("Maximum number is:"+maxi(n, arr));
     }
 }
+#include<bits/stdc++.h>
+using namespace std;
+int coinChange(vector<int>& coins, int amount) {
+    vector<int> dp(amount + 1, INT_MAX);
+    dp[0] = 0;
+    for (int coin : coins) {
+        for (int i = coin; i <= amount; ++i) {
+            if (dp[i - coin] != INT_MAX) {
+                dp[i] = min(dp[i], dp[i - coin] + 1);
+            }
+        }
+    }
+
+    return dp[amount] == INT_MAX ? -1 : dp[amount];
+}
+
+int main() {
+    int n;
+    cout<<"Enter the number of coins: ";
+    cin>>n;
+    vector<int> coins (n);
+    cout<<"Enter "<<n<<" coin: ";
+    for(int i=0;i<n;i++){
+        cin>>coins[i];
+    }
+    
+    int amount;
+    cout<<"Enter the amount: ";
+    cin>>amount;
+
+    int result = coinChange(coins, amount);
+
+    if (result != -1) {
+        cout << "Minimum number of coins needed: " << result << endl;
+    } else {
+        cout << "It's not possible to make the amount with given coins." << endl;
+    }
+    return 0;
+}
