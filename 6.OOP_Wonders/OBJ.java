9e965acd1cff287d5a1b764ed068b402020399ca
unknown <mdshahreerirfan@gmail.com>
2023-11-27 00:14:32 +0600

    Encapsulation

---
+++ 6.OOP_Wonders/OBJ.java
@@ -0,0 +1,43 @@
+class Rectangle{
+    private double length ,width;
+    
+}
+
+class Employee{
+    private String name,id;
+    private int salary;
+    
+}
+
+class Circle{
+    private double radious;
+    
+}
+
+class Car{
+    private String brand,model;
+    private int year;
+}
+
+class Student{
+    private String name, rollNumber,program ;
+    private int batch;
+
+    
+}
+
+public class OBJ {
+    public static void main(String[] args) {
+        Rectangle rectrangle = new Rectangle();
+
+        Employee employee = new Employee();
+
+        Circle circle = new Circle();
+
+
+        Car car = new Car();
+
+        Student student  = new Student();
+
+    }
+}
